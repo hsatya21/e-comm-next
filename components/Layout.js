a67686ce9fed82932c847d715d6491835f57cbf1
Harshit Satya <hsatya@systango.com>
2023-05-12 14:01:13 +0530

    created the layout for web app

---
+++ components/Layout.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              amazona
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2023 amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
